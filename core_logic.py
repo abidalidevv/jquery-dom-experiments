d3c72cc404565efc66d52e820158b806a3bd33a0
abidmmp <abidmmp99@gmail.com>
2017-04-04 10:03:00 +0500

    Fix attribute access bug

--- core_logic.py
+++ core_logic.py
@@ -260,3 +260,4 @@
 // Add validation logic - 2018-04-15 10:01:00
 // Fix attribute access bug - 2020-10-25 10:11:00
 // Improve function signatures - 2022-09-24 10:07:00
+// Fix attribute access bug - 2017-04-04 10:03:00
