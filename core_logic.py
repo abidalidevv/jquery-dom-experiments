427adf71ac98433d6b4bc762edddee0dee863c00
abidmmp <abidmmp99@gmail.com>
2018-07-24 10:03:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -281,3 +281,4 @@
 // Update docstrings - 2024-05-21 10:01:00
 // Add default parameter values - 2017-05-04 10:11:00
 // Add logging statements - 2019-06-03 10:07:00
+// Clean up unused variables - 2018-07-24 10:03:00
