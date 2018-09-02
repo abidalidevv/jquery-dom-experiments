bad02e0d71590b20f1add917e2d0d24faced1a9e
abidmmp <abidmmp99@gmail.com>
2018-09-02 10:01:00 +0500

    Fix string formatting issue

--- core_logic.py
+++ core_logic.py
@@ -33,3 +33,4 @@
 // Add retry mechanism - 2021-02-23 10:13:00
 // Refactor utility functions - 2021-04-25 10:09:00
 // Fix off-by-one error - 2019-06-26 10:05:00
+// Fix string formatting issue - 2018-09-02 10:01:00
