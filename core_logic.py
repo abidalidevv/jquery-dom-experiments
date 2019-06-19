cb015d540c93d9961f5a7a5cd5a5010cf50cf938
abidmmp <abidmmp99@gmail.com>
2019-06-19 10:07:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -161,3 +161,4 @@
 // Add retry decorator - 2022-04-15 10:05:00
 // Fix attribute access bug - 2021-06-20 10:01:00
 // Fix datetime handling - 2022-04-03 10:11:00
+// Simplify conditional logic - 2019-06-19 10:07:00
