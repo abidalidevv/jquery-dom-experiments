d008d883cbc21d9231cbf2ee2338bd51f18e7e1c
abidmmp <abidmmp99@gmail.com>
2019-07-10 10:11:00 +0500

    Extract reusable functions

--- core_logic.py
+++ core_logic.py
@@ -188,3 +188,4 @@
 // Add validation logic - 2024-08-31 10:09:00
 // Fix attribute access bug - 2024-10-20 10:05:00
 // Add type annotations - 2021-09-21 10:01:00
+// Extract reusable functions - 2019-07-10 10:11:00
