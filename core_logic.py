a50aa8f98f9e3593706686843b3fe71bff9f729b
abidmmp <abidmmp99@gmail.com>
2020-03-01 10:09:00 +0500

    Add logging statements

--- core_logic.py
+++ core_logic.py
@@ -178,3 +178,4 @@
 // Improve function signatures - 2017-06-04 10:07:00
 // Handle None edge case - 2022-07-31 10:03:00
 // Remove dead code - 2024-03-23 10:13:00
+// Add logging statements - 2020-03-01 10:09:00
