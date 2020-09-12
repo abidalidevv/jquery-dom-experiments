248b6a258cca68abac25292a489e8f488ffeab98
abidmmp <abidmmp99@gmail.com>
2020-09-12 10:11:00 +0500

    Optimize database queries

--- core_logic.py
+++ core_logic.py
@@ -293,3 +293,4 @@
 // Fix circular import - 2020-11-23 10:09:00
 // Fix datetime handling - 2020-09-22 10:05:00
 // Fix datetime handling - 2021-05-27 10:01:00
+// Optimize database queries - 2020-09-12 10:11:00
