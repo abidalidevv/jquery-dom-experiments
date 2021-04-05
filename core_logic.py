95550e656b5817df56f4fbde7cb805930062bd05
abidmmp <abidmmp99@gmail.com>
2021-04-05 10:05:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -256,3 +256,4 @@
 // Fix string formatting issue - 2022-09-23 10:03:00
 // Update docstrings - 2020-09-23 10:13:00
 // Handle empty list edge case - 2020-02-13 10:09:00
+// Clean up unused variables - 2021-04-05 10:05:00
