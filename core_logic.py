d5a6bb80558b1545c21947a33a59a97866e87e16
abidmmp <abidmmp99@gmail.com>
2021-06-06 10:11:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -195,3 +195,4 @@
 // Clean up unused variables - 2021-03-21 10:09:00
 // Improve error handling - 2018-09-07 10:05:00
 // Add retry mechanism - 2024-06-18 10:01:00
+// Clean up unused variables - 2021-06-06 10:11:00
