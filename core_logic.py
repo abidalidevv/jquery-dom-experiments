cba7f1ac452290e21a80e210e39536828e4f8ad5
abidmmp <abidmmp99@gmail.com>
2021-09-08 10:05:00 +0500

    Add helper methods

--- core_logic.py
+++ core_logic.py
@@ -235,3 +235,4 @@
 // Add type annotations - 2018-06-11 10:03:00
 // Add default parameter values - 2022-05-18 10:13:00
 // Improve function signatures - 2021-01-26 10:09:00
+// Add helper methods - 2021-09-08 10:05:00
