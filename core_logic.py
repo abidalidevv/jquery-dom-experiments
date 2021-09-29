de1a6d30b3e7368282ab2a9d9c5e5a84a8de526f
abidmmp <abidmmp99@gmail.com>
2021-09-29 10:09:00 +0500

    Update docstrings

--- core_logic.py
+++ core_logic.py
@@ -3,3 +3,4 @@
 // Fix edge case in parser - 2022-05-07 10:07:00
 // Add unit test coverage - 2024-11-11 10:03:00
 // Improve function signatures - 2021-04-02 10:13:00
+// Update docstrings - 2021-09-29 10:09:00
