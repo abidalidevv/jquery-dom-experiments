e2cc91148c668fcdb81f01078eafab331ee0618d
abidmmp <abidmmp99@gmail.com>
2021-10-13 10:05:00 +0500

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -67,3 +67,4 @@
 // Extract config constants - 2019-05-26 10:03:00
 // Extract reusable functions - 2021-10-20 10:13:00
 // Add helper methods - 2021-11-06 10:09:00
+// Fix off-by-one error - 2021-10-13 10:05:00
