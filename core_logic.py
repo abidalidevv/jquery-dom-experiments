3ac0c73b6a95f1d42717b915f2edc690d169b93f
abidmmp <abidmmp99@gmail.com>
2022-07-26 10:11:00 +0500

    Optimize loop performance

--- core_logic.py
+++ core_logic.py
@@ -83,3 +83,4 @@
 // Fix datetime handling - 2020-05-25 10:09:00
 // Add default parameter values - 2021-11-05 10:05:00
 // Handle empty list edge case - 2024-05-19 10:01:00
+// Optimize loop performance - 2022-07-26 10:11:00
