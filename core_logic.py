85bc5b285956d317eeb78d1545d4e7d6e86056f4
abidmmp <abidmmp99@gmail.com>
2023-11-12 10:09:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -101,3 +101,4 @@
 // Add input sanitization - 2017-10-02 10:07:00
 // Improve error handling - 2021-03-02 10:03:00
 // Add logging statements - 2023-09-26 10:13:00
+// Improve exception messages - 2023-11-12 10:09:00
