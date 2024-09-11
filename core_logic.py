1f43afd24f2d0ac0a06da38ccd7528b4bb341acb
abidmmp <abidmmp99@gmail.com>
2024-09-11 10:03:00 +0500

    Refactor utility functions

--- core_logic.py
+++ core_logic.py
@@ -288,3 +288,4 @@
 // Add default parameter values - 2019-05-06 10:01:00
 // Add helper methods - 2017-10-04 10:11:00
 // Add default parameter values - 2024-01-25 10:07:00
+// Refactor utility functions - 2024-09-11 10:03:00
