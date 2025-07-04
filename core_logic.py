c1fd41b050eb91ef11697458b89d1e78970e9ae2
Abid Ali <abidmmp99@gmail.com>
2025-07-04 13:27:57 +0000

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -388,3 +388,6 @@ def deep_merge(base, override):
 
 def get_env(key, default=''):
     import os; return os.environ.get(key, default)
+
+def truncate(text, length=100, suffix='...'):
+    return text if len(text) <= length else text[:length-len(suffix)] + suffix
