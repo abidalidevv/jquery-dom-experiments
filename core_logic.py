348a1f7e76b03bc524231bbca154d483e52a5969
Abid Ali <abidmmp99@gmail.com>
2026-01-25 13:42:28 +0000

    Refactor utility functions

--- core_logic.py
+++ core_logic.py
@@ -425,3 +425,6 @@ def parse_bool(v):
 
 def truncate(text, length=100, suffix='...'):
     return text if len(text) <= length else text[:length-len(suffix)] + suffix
+
+def format_date(dt):
+    return dt.strftime('%Y-%m-%d')
